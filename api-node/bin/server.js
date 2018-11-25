9a066556abcd3a7de9cf55bb77469aa0165c1fe3
rodolfogomes <rodolfoaguiargomes@gmail.com>
2018-11-25 16:06:46 -0300

    estruturando aplicação

--- api-node/bin/server.js
+++ api-node/bin/server.js
@@ -1,23 +1,12 @@
+const app = require('../src/app');
 const http = require('http');
 const debug = require('debug')('apinode:server');
-const express = require('express');
 
-const app = express();
 const port = normalizePort(process.env.PORT ||'3000');
 
 app.set('port',port);
 
 const server = http.createServer(app);
-const router = express.Router();
-
-const route = router.get('/',(req,res,next)=>{
-    res.status(200).send({
-        title:'Node Api',
-        version: '0.0.1'
-    });
-})
-
-app.use('/',route)
 
 server.listen(port)
 console.log('Servidor roddanddo...')

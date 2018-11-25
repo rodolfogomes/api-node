5cad4d6f263c6d0af839d485dec628bd50824125
rodolfogomes <rodolfoaguiargomes@gmail.com>
2018-11-25 15:37:28 -0300

    normalizando porta

--- api-node/server.js
+++ api-node/server.js
@@ -3,7 +3,7 @@ const debug = require('debug')('apinode:server');
 const express = require('express');
 
 const app = express();
-const port = 3000;
+const port = normalizePort(process.env.PORT ||'3000');
 
 app.set('port',port);
 
@@ -21,3 +21,17 @@ app.use('/',route)
 
 server.listen(port)
 console.log('Servidor roddanddo...')
+
+function normalizePort(val){
+    const port = parseInt(val,10);
+
+    if(isNaN(port)){
+        return val;
+    }
+
+    if(port >= 0){
+        return port;
+    }
+
+    return false;
+}

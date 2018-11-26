d3f0d41311993375e083e47243c284e354f743a4
rodolfogomes <rodolfoaguiargomes@gmail.com>
2018-11-26 09:08:00 -0300

    Fabrica de conexões

---
+++ api-node/bin/connectionFactory.js
@@ -0,0 +1,18 @@
+
+const { Pool } = require('pg')
+function connection() {
+  var pool = new Pool({
+    host: 'localhost',
+    port: 5432,
+    user: 'postgres',
+    password: 'postgres',
+    database: 'apinode'
+  })
+  return pool
+}
+module.exports = function () {
+  query: (text, params, callback) => {
+    return pool.query(text, params, callback)
+  }
+  return connection;
+}
